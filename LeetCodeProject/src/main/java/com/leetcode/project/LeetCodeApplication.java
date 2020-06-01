95c221d862bbd80fc49ec42b5e6db683c6acb8b5
Tcindy <945991614@qq.com>
2020-06-01 23:49:16 +0800

    力扣算法

---
+++ LeetCodeProject/src/main/java/com/leetcode/project/LeetCodeApplication.java
@@ -0,0 +1,12 @@
+package com.leetcode.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetCodeApplication {
+    public static void main(String[] args) {
+        System.out.println("===================启动 leetcodeApplication =============");
+        SpringApplication.run(LeetCodeApplication.class);
+    }
+}
